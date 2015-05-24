e37e9c87d06c02d8b168306c25e93f499b27f3c1
Rob Passmore <robertmpassmore@gmail.com>
2015-05-24 23:21:00 +0200

    maybe

--- robsscript.py
+++ robsscript.py
@@ -1,9 +1,14 @@
-path = "/Users/rob/dev/IR/newdocs"
-list_of_files = {}
-for (dirpath, dirnames, filenames) in os.walk(path):
-    for filename in filenames:
-        if filename.contains('Beeld~'): 
-            list_of_files[filename] = os.sep.join([dirpath, filename])
+# path = "/Users/rob/dev/IR/newdocs"
+# list_of_files = {}
+# for (dirpath, dirnames, filenames) in os.walk(path):
+#     for filename in filenames:
+#         if filename.contains('Beeld~'): 
+#             list_of_files[filename] = os.sep.join([dirpath, filename])
 
-for a in list_of_files:
-	print(a)
+# for a in list_of_files:
+# 	print(a)
+
+mypath = os.getcwd()
+from os import listdir
+from os.path import isfile, join
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
